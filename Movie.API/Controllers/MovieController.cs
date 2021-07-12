fac49c40f55d01e8e974c6cfc80f92aa84ee5e8c
paulchek777@hotmail.com <paulchek777@hotmail.com>
2021-07-13 00:08:09 +0700

    Add project files.

---
+++ Movie.API/Controllers/MovieController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Movie.API.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Movie.API.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase {
+        private readonly MovieContext _movieContext;
+
+        public MovieController(MovieContext movieContext) {
+            _movieContext = movieContext;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Model.Movie>>> Get() {
+            return Ok(await _movieContext.Movies.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Model.Movie>>> Get(int id) {
+            return Ok(await _movieContext.Movies.FirstOrDefaultAsync(v=>v.Id == id));
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<Model.Movie>> PostMovie(Model.Movie movie) {
+          _movieContext.Movies.Add(movie);
+            await _movieContext.SaveChangesAsync();
+
+            return CreatedAtAction("Get", new { id = movie.Id }, movie);
+        }
+    }
+}
---
+++ Movie.API/Model/MovieContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Movie.API.Model {
+    public class MovieContext : DbContext {
+        public MovieContext(DbContextOptions options) : base(options) {
+        }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
